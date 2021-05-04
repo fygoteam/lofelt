61b708a84dc09bba7880642f1ce3a3eada0ce7a5
Daniel Ocampo <danielocampo2@users.noreply.github.com>
2021-05-04 10:53:12 -0300

    Fix config path

--- ios/import_frameworks.sh
+++ ios/import_frameworks.sh
@@ -9,10 +9,10 @@
 
 echo "${PODS_TARGET_SRCROOT}"
 echo "$CONFIGURATION"
-echo "${LOFELT_CONFIG}"
+config=${$CONFIGURATION"%-*}
 
 
 rm -rf "${PODS_TARGET_SRCROOT}/LofeltHaptics.framework"
 
 
-cp -R "${PODS_TARGET_SRCROOT}/Environements/${LOFELT_CONFIG}/LofeltHaptics.framework" "${PODS_TARGET_SRCROOT}"
+cp -R "${PODS_TARGET_SRCROOT}/Environements/$config/LofeltHaptics.framework" "${PODS_TARGET_SRCROOT}"
