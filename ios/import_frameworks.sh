05cf6f986ded28f7800cd36ee0fc885e7cad5656
Daniel Ocampo <danielocampo2@users.noreply.github.com>
2021-05-04 10:13:12 -0300

    Use user defined lofelt config to fix issue with flavors

--- ios/import_frameworks.sh
+++ ios/import_frameworks.sh
@@ -9,9 +9,10 @@
 
 echo "${PODS_TARGET_SRCROOT}"
 echo "$CONFIGURATION"
+echo "$LOFELT_CONFIG"
 
 
 rm -rf "${PODS_TARGET_SRCROOT}/LofeltHaptics.framework"
 
 
-cp -R "${PODS_TARGET_SRCROOT}/Environements/$CONFIGURATION/LofeltHaptics.framework" "${PODS_TARGET_SRCROOT}"
+cp -R "${PODS_TARGET_SRCROOT}/Environements/$LOFELT_CONFIG/LofeltHaptics.framework" "${PODS_TARGET_SRCROOT}"
